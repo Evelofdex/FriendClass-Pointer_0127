97c64c0270db458b412232427ff04960ec8bdb4b
Evelofdex <evelofdex@gmail.com>
2026-04-28 18:37:47 +0700

    membuat file friendclass

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -3,24 +3,6 @@
 using namespace std;
 
 class siswa;
-class Node
-{
-    public:
-    int noMhs;
-    Node *next;
-    Node *prev;
-};
-
-class DoublyLinkedList
-{
-    private:
-    Node *START;
-
-    public:
-    DoublyLinkedList(){
-
-    }
-};
 
 int main(){
 
