ed77699b7d3c87eb5fa053729488c20bb6d8770f
Evelofdex <evelofdex@gmail.com>
2026-04-28 18:41:00 +0700

    menambahkan isi member untuk class siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 class siswa;
-class orang
-{
+class orang{
     private:
     string nama;
 
     public:
     void setNama(string pNama);
     friend class siswa;
+};
+
+class siswa{
+    private:
+    int id;
 };
 
 int main(){
